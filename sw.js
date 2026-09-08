e99f1c25888cc71bc42953382b4c7696658c82f0
nany.kim <72008368+kyungnankim@users.noreply.github.com>
2026-09-08 14:03:31 +0900

    Cache all vocabulary data for offline study

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'toeic-30day-v3'
-const CORE = ['/', '/index.html', '/app.js', '/style.css', '/manifest.webmanifest', '/icon.svg', '/data/words-01-05.json', '/data/words-06-10.json', '/data/words-11-15.json', '/data/words-16-20.json', '/data/words-21-25.json', '/data/words-26-30.json']
+const CACHE = 'toeic-30day-v4'
+const CORE = [
+  '/', '/index.html', '/app.js', '/style.css', '/manifest.webmanifest', '/icon.svg',
+  '/data/words-01-03.json', '/data/words-04-06.json', '/data/words-07-09.json',
+  '/data/words-10-12.json', '/data/words-13-15.json', '/data/words-16-18.json',
+  '/data/words-19-21.json', '/data/words-22-24.json', '/data/words-25-27.json',
+  '/data/words-28-30.json'
+]
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)))
